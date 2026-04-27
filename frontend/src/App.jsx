538749ac61f216b4a380b6ec33dbdd08e8483643
Silsebas <jordancubillo@gmail.com>
2026-04-27 17:29:14 -0600

    fix: agregar ruta reset-password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home'; // 👈 Aquí usamos el nuevo
+import Home from './components/Home/Home'; 
 import Login from './components/Login/Login'; 
 import Dashboard from './components/Dashboard/Dashboard';
 import ActivarCuenta from './components/ActivarCuenta/ActivarCuenta';
+// 🚨 Asegúrate de importar el componente de Reset Password aquí
+import NuevoPassword from './components/NuevoPassword/NuevoPassword'; 
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/activar-cuenta/:token" element={<ActivarCuenta />} />
+        <Route path="/reset-password/:token" element={<NuevoPassword />} />
       </Routes>
     </Router>
   );
